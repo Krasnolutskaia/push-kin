640fe45e10886d9ec3395b04625aa17ffebace92
E. Krasnolutskaia <elizavetakrasnolutskaya@yandex.ru>
2023-10-02 22:38:49 +0300

    Alphabet order

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,12 @@
 
 int main()
 {
-    char str[] = "C:\\Users\\eliza\\VSCodeProjects\\push-kin\\src\\file.txt";
+    char str[] = ".\\src\\file.txt";
     Text text = read_file(str);
 
-    printf("aaa %d\n", find_min_index(text.data, text.nlines, sizeof(char *), str_cmp));
+    // printf("aaa %d\n", find_min_index(text.data, text.nlines, sizeof(char *), str_cmp));
 
-    selection_sort(text.data, text.nlines, sizeof(char *), reverse_str_cmp_letters);
+    selection_sort(text.data, text.nlines, sizeof(char *), reverse_str_cmp_letters); // reverse_str_cmp     reverse_str_cmp_letters     str_cmp     str_cmp_letters
 
     print_text(&text);
 
